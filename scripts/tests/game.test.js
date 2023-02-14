5b6a9424baa0b277786ab84fb3949c6e0317e1b9
ozlemhafalir <ozlemoztep@gmail.com>
2023-02-14 10:31:02 +0000

    start game structure with testing

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,17 @@
+const { exportAllDeclaration } = require("@babel/types");
+const { default: test } = require("node:test");
+const { describe } = require("yargs");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+});
