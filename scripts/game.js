6cb716cf29db0e5f47c400186e65feb21b7a562d
ozlemhafalir <ozlemoztep@gmail.com>
2023-02-15 12:29:50 +0000

    add more advanced functions

--- scripts/game.js
+++ scripts/game.js
@@ -19,5 +19,11 @@ function addTurn() {
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
     //showTurns();
 }
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
 
-module.exports = { game, newGame, showScore, addTurn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
