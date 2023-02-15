81f661fedad626837dbbb157ea0683c40edab29f
ozlemhafalir <ozlemoztep@gmail.com>
2023-02-15 11:03:56 +0000

    try more advanced tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,23 @@
 let game = {
     currentGame: [],
     score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
 };
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+function showScore() {
+   document.getElementById("score").innerText = game.score;
+}
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns();
+}
 
-
-module.exports = { game };
+module.exports = { game, newGame, showScore, addTurn };
